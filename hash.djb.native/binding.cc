7d896dcce8c2978b282ec978dd6ecc327ca0f1e7
Alan Gutierrez <alan@prettyrobots.com>
2013-08-18 14:28:35 -0400

    Invoke native DJB constructor.
    Invoke the native DJB constructor raising an exception immediately.
    
    See #32.

--- hash.djb.native/binding.cc
+++ hash.djb.native/binding.cc
@@ -1,15 +1,55 @@
 #include <node.h>
+#include <node_object_wrap.h>
 #include <v8.h>
+
+extern "C" {
+extern void* hash_allocate (uint32_t seed);
+extern void hash_free (void* hash);
+extern int hash_block_size();
+extern void hash_update(void* hash, void* key, int len);
+extern void hash_remainder(void* hash, void* out);
+}
 
 using namespace v8;
 
-Handle<Value> Method(const Arguments& args) {
-  HandleScope scope;
-  return scope.Close(String::New("world"));
+class Hash : public node::ObjectWrap {
+public:
+    static void Initialize(Handle<Object> target);
+
+protected:
+    static Handle<Value>  New(const Arguments& args);
+   /*static void HashUpdate(const FunctionCallbackInfo<Value>& args);
+    static void HashDigest(const FunctionCallbackInfo<Value>& args);*/
+
+    Hash () : hash_(NULL) {
+    }
+
+    ~Hash () {
+        if (hash_) hash_free(hash_);
+    }
+
+private:
+    void* hash_;
+};
+
+void Hash::Initialize (Handle<Object> target) {
+    HandleScope scope;
+
+    Local<FunctionTemplate> t = FunctionTemplate::New(New);
+
+    t->InstanceTemplate()->SetInternalFieldCount(1);
+
+    target->Set(String::New("Hash"), t->GetFunction());
+}
+
+Handle<Value> Hash::New (const Arguments& args) {
+    HandleScope scope;
+
+   return ThrowException(Exception::Error(String::New("Must give hashtype string as argument")));
 }
 
 void init(Handle<Object> target) {
-  NODE_SET_METHOD(target, "hello", Method);
+  Hash::Initialize(target);
 }
 
 NODE_MODULE(binding, init);
